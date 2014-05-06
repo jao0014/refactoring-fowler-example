9bea9d90a2d9343a734116298649f1d3c7f543dd
jao0014 <jao0014@alu.ubu.es>
2014-05-06 17:12:38 +0200

    Crear campo en la clase original para referenciar el objeto estrategia

--- src/ubu/gii/dass/refactoring/Movie.java
+++ src/ubu/gii/dass/refactoring/Movie.java
@@ -1,38 +1,72 @@
 package ubu.gii.dass.refactoring;
+
 /**
- * Tema Refactorizaciones
- * 
- * Ejemplo de aplicación de refactorizaciones. Actualizado para colecciones
- * genéricas de java 1.5
- * 
- * @author M. Fowler y <A HREF="mailto:clopezno@ubu.es">Carlos López</A>
- * @version 1.1
- * @see java.io.File
- * 
- */
+* Tema  Refactorizaciones 
+*
+* Ejemplo de aplicación de refactorizaciones. Actualizado para colecciones genéricas de java 1.5
+*
+* @author M. Fowler y <A HREF="mailto:clopezno@ubu.es">Carlos López</A>
+* @version 1.1
+* @see java.io.File
+*
+*/
 
 public class Movie {
-	public static final int CHILDRENS = 2;
-	public static final int REGULAR = 0;
-	public static final int NEW_RELEASE = 1;
+      public static final int  CHILDRENS = 2;
+      public static final int  REGULAR = 0;
+      public static final int  NEW_RELEASE = 1;
 
-	private String _title;
-	private int _priceCode;
+      private String _title;
+      private int _priceCode;
 
-	public Movie(String title, int priceCode) {
-		_title = title;
-		_priceCode = priceCode;
+      public Movie(String title, int priceCode) {
+        _title = title;
+        _priceCode = priceCode;
+      }
+      public int getPriceCode() {
+        return _priceCode;
+      }
+      public void setPriceCode(int arg) {
+        _priceCode = arg;
+      }
+      public String getTitle () {
+        return _title;
+      }
+	/**
+	 * @param thisAmount
+	 * @return
+	 */
+	public double getCharge(int daysRented) {
+		double thisAmount = 0;
+		//determine amounts for each line
+		   switch (getPriceCode()) {
+		     case Movie.REGULAR:
+		       thisAmount += 2;
+		       if (daysRented > 2)
+		         thisAmount += (daysRented - 2) * 1.5;
+		     break;
+		     case Movie.NEW_RELEASE:
+		       thisAmount += daysRented * 3;
+		     break;
+		     case Movie.CHILDRENS:
+		       thisAmount += 1.5;
+		       if (daysRented > 3)
+		         thisAmount += (daysRented - 3) * 1.5;
+		      break;
+		    }
+		return thisAmount;
 	}
-
-	public int getPriceCode() {
-		return _priceCode;
+	/**
+	 * @param frequentRenterPoints
+	 * @return
+	 */
+	public int getFrecuentRenterPoints(int daysRented) {
+		int frequentRenterPoints=1;
+		// add bonus for a two day new release rental
+	    if ((getPriceCode() == Movie.NEW_RELEASE) &&
+	         daysRented > 1)
+	        frequentRenterPoints ++;
+		return frequentRenterPoints;
 	}
+    }
 
-	public void setPriceCode(int arg) {
-		_priceCode = arg;
-	}
-
-	public String getTitle() {
-		return _title;
-	}
-}
--- src/ubu/gii/dass/refactoring/VideoClubTest.java
+++ src/ubu/gii/dass/refactoring/VideoClubTest.java
@@ -2,57 +2,83 @@ package ubu.gii.dass.refactoring;
 import junit.framework.*;
 
 /**
- * Tema Refactorizaciones
- * 
- * Ejemplo de aplicación de refactorizaciones. Actualizado para colecciones
- * genéricas de java 1.5
- * 
- * @author M. Fowler y <A HREF="mailto:clopezno@ubu.es">Carlos López</A>
- * @version 1.1
-
- * 
- */
+* Tema  Refactorizaciones 
+*
+* Ejemplo de aplicación de refactorizaciones. Actualizado para colecciones genéricas de java 1.5
+*
+* @author M. Fowler y <A HREF="mailto:clopezno@ubu.es">Carlos López</A>
+* @version 1.1
+* @see java.io.File
+*
+*/
 public class VideoClubTest extends TestCase {
 	protected Movie m0, m11, m12, m2;
 	protected Customer c1;
-
+	
 	protected void setUp() {
-		m11 = new Movie("Sky Captain", 1);
-		m12 = new Movie("Alejandro Magno", 1);
+	    m11 = new Movie("Sky Captain",1);
+		m12 = new Movie("Alejandro Magno",1);
 		m0 = new Movie("Accion Mutante", 0);
-		m2 = new Movie("Hermano Oso", 2);
-
+		m2 = new Movie("Hermano Oso",2);
+		
 		c1 = new Customer("Manuel");
 	}
-
-
 	public static Test suite() {
 
 		return new TestSuite(VideoClubTest.class);
 	}
-
 	public void testAlquiler() {
 
-		Rental r1 = new Rental(m11, 5);
-		Rental r2 = new Rental(m0, 1);
-		Rental r3 = new Rental(m2, 10);
-
+		Rental r1 = new Rental(m11,5);
+		Rental r2 = new Rental(m0,1);
+		Rental r3 = new Rental(m2,10);
+		
 		c1.addRental(r1);
 		c1.addRental(r2);
 		c1.addRental(r3);
-
-		String salida = c1.statement();
-
-		String salidaEsperada = new String("Rental Record for Manuel\n"
-				+ "\tSky Captain\t15.0\n" + "\tAccion Mutante\t2.0\n"
-				+ "\tHermano Oso\t12.0\n" + "Amount owed is 29.0\n"
-				+ "You earned 4 frequent renter points");
-
-		assertTrue("Calcula mal el alquiler", salidaEsperada.equals(salida));
-
+		
+		String salida= c1.statement();
+		
+		String salidaEsperada = new String ("Rental Record for Manuel\n" +
+		"\tSky Captain\t15.0\n"+
+        "\tAccion Mutante\t2.0\n" +
+        "\tHermano Oso\t12.0\n" +
+        "Amount owed is 29.0\n" +
+        "You earned 4 frequent renter points");
+ 
+        assertTrue("Calcula mal el alquiler",salidaEsperada.equals(salida));
+        
 	}
+	
+	public void testAlquilerHtml() {
 
-	public static void main(String[] args) {
+		Rental r1 = new Rental(m11,5);
+		Rental r2 = new Rental(m0,1);
+		Rental r3 = new Rental(m2,10);
+		
+		c1.addRental(r1);
+		c1.addRental(r2);
+		c1.addRental(r3);
+		
+		String salida= c1.htmlStatement();
+		
+		
+		String salidaEsperada = new String ("<H1>Rentals for <EM>Manuel</EM></H1><P>\n" +
+		"Sky Captain: 15.0<BR>\n"+
+        "Accion Mutante: 2.0<BR>\n" +
+        "Hermano Oso: 12.0<BR>\n" +
+        "<P>You owe <EM>29.0</EM><P>\n" +
+        "On this rental you earned <EM>4</EM> frequent renter points<P>");
+		
+		System.out.println(salida);
+		System.out.println(salidaEsperada);
+		
+ 
+        assertTrue("Calcula mal el alquiler",salidaEsperada.equals(salida));
+        
+	}
+	
+	public static void main (String[] args) {
 		junit.textui.TestRunner.run(suite());
 	}
 }
